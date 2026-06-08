3a267d1696fcc3189b1590df093c08a41dd6690d
Syed Shadab Hussain <shadabhussainara@gmail.com>
2026-06-08 15:11:52 +0530

    Image Zoom

--- components/tutorials/ImageBlock.tsx
+++ components/tutorials/ImageBlock.tsx
@@ -1,7 +1,7 @@
 "use client";
+
+import { useState } from "react";
 import Image from "next/image";
-import Zoom from "react-medium-image-zoom";
-import "react-medium-image-zoom/dist/styles.css";
 
 interface ImageBlockProps {
   src: string;
@@ -16,29 +16,48 @@ export default function ImageBlock({
   caption,
   priority = false,
 }: ImageBlockProps) {
-  return (
-    <figure className="my-8">
-      <Zoom>
-        <Image
-          src={src}
-          alt={alt}
-          width={1200}
-          height={675}
-          quality={85}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 90vw, 1200px"
-          priority={priority}
-          className="w-full h-auto rounded-xl border border-gray-200 cursor-zoom-in"
-        />
-      </Zoom>
+  const [open, setOpen] = useState(false);
 
-      {caption && (
-        <figcaption className="text-center text-sm text-gray-500 mt-3">
-          {caption}
-          <div className="text-xs text-gray-400 mt-1">
-            Click image to enlarge
-          </div>
-        </figcaption>
+  return (
+    <>
+      <figure className="my-8">
+        <div onClick={() => setOpen(true)} className="cursor-zoom-in">
+          <Image
+            src={src}
+            alt={alt}
+            width={1200}
+            height={675}
+            quality={85}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 90vw, 1200px"
+            priority={priority}
+            className="w-full h-auto rounded-xl border border-gray-200"
+          />
+        </div>
+
+        {caption && (
+          <figcaption className="text-center text-sm text-gray-500 mt-3">
+            {caption}
+            <div className="text-xs text-gray-400 mt-1">
+              Click image to enlarge
+            </div>
+          </figcaption>
+        )}
+      </figure>
+
+      {open && (
+        <div
+          onClick={() => setOpen(false)}
+          className="fixed inset-0 z-50 bg-black/80 flex items-center justify-center p-4"
+        >
+          <Image
+            src={src}
+            alt={alt}
+            width={2000}
+            height={1200}
+            className="max-w-[95vw] max-h-[95vh] w-auto h-auto rounded-lg"
+          />
+        </div>
       )}
-    </figure>
+    </>
   );
 }
